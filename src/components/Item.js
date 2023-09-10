8c550b6e96cbfd825efcbdeb7036dcda2e8d5efe
BenjaminJacobson <ben.jacobson@flatironschool.com>
2023-09-10 16:45:28 -0400

    lab completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [ itemClassName, setitemClassName] = useState(false)
+  const itemClass = itemClassName ? "in-cart" : ""
+
+  function clickHandler(){
+    console.log("I was clicked")
+    setitemClassName(!itemClassName)
+  }
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={clickHandler} className={itemClassName?"remove" :"add"}>{itemClassName?"Remove from Cart" :"Add to Cart"}</button>
     </li>
   );
 }
